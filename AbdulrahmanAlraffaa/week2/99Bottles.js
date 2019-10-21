6599b22d72d2f332ec37000e787e7e7af86cbcba
araffaa <abdulrahman.alraffaa@gmail.com>
2019-10-21 20:33:48 +0300

    Homework02
    HW02 final version

---
+++ AbdulrahmanAlraffaa/week2/99Bottles.js
@@ -0,0 +1,21 @@
+let i;
+for(i=99;i>0;i--){
+
+    if(i>2){
+    console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.
+    Take one down and pass it around, ${i-1} bottles of beer on the wall.`)
+    }
+    else if (i==2){
+        console.log(`${i} bottles of beer on the wall, ${i} bottles of beer.
+        Take one down and pass it around, ${i-1} bottle of beer on the wall.`)
+    }
+    else if (i==1){
+
+        console.log(`${i} bottle of beer on the wall, ${i} bottle of beer.
+        Take one down and pass it around, no more bottles of beer on the wall.`)
+    }
+
+}
+
+console.log(`No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.`);
---
+++ AbdulrahmanAlraffaa/week2/RandomAddress.js
@@ -0,0 +1,12 @@
+let streetNumber=['882','8928','52','7878','579']
+let streetName=['1st Street','Front Ave','W. Canal Street','Summerhouse Circle','Meadow St']
+let cityName=['Brandon','Northville','Alexandria','Strongsville','Newburgh']
+let stateName=[`FL`,`MI`,`VA`,`OH`,`NY`]
+let zipCode=[`33510`,`48167`,`22304`,`12550`,`44136`]
+let rand1=Math.floor(Math.random()*5)
+let rand2=Math.floor(Math.random()*5)
+let rand3=Math.floor(Math.random()*5)
+let rand4=Math.floor(Math.random()*5)
+let rand5=Math.floor(Math.random()*5)
+
+console.log(`${streetNumber[rand1]} ${streetName[rand2]},${cityName[rand3]} ${stateName[rand4]} ${zipCode[rand5]}`);
