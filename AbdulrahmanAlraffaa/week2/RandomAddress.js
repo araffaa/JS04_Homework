b1213e5ca75351ccfda764642bf36cd760da0a02
araffaa <abdulrahman.alraffaa@gmail.com>
2019-10-21 20:41:34 +0300

    Homework02
    Final version 1.0

--- AbdulrahmanAlraffaa/week2/RandomAddress.js
+++ AbdulrahmanAlraffaa/week2/RandomAddress.js
@@ -3,10 +3,10 @@ let streetName=['1st Street','Front Ave','W. Canal Street','Summerhouse Circle',
 let cityName=['Brandon','Northville','Alexandria','Strongsville','Newburgh']
 let stateName=[`FL`,`MI`,`VA`,`OH`,`NY`]
 let zipCode=[`33510`,`48167`,`22304`,`12550`,`44136`]
-let rand1=Math.floor(Math.random()*5)
-let rand2=Math.floor(Math.random()*5)
-let rand3=Math.floor(Math.random()*5)
-let rand4=Math.floor(Math.random()*5)
-let rand5=Math.floor(Math.random()*5)
+let rand1=Math.floor(Math.random()*streetNumber.length)
+let rand2=Math.floor(Math.random()*streetName.length)
+let rand3=Math.floor(Math.random()*cityName.length)
+let rand4=Math.floor(Math.random()*stateName.length)
+let rand5=Math.floor(Math.random()*zipCode.length)
 
 console.log(`${streetNumber[rand1]} ${streetName[rand2]},${cityName[rand3]} ${stateName[rand4]} ${zipCode[rand5]}`);
